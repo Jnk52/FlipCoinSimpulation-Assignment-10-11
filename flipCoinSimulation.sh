668268796ddad63a9f178b4be58d5bf6de9a5ed0
jnk52 <ajienkyanikam52@gmail.com>
2020-05-14 13:32:26 -0700

    uc3

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,21 +1,26 @@
 #!/bin/bash -x
 
-read -p "Enter Times to flip: " num
 heads=0
 tails=0
-count=0
-while [ $count -ne $num ]
+while [ $tails -le 21 -o $heads -le 21 ]
 do
-	$((count++))
 	result=$((RANDOM % 2))
-	if [ $result -eq 0 ]
-	then
+if [ $result -eq 0 ]
+then
 		$((tails++))
 	else
 		$((heads++))
 	fi
-		
+              break
 done
-echo "Heads: " $heads
-echo "Tails: " $tails
 
+if [$heads -eq $tails ]
+then
+	echo "Its a tie !"
+elif [ $heads -gt $tails ]
+then
+	echo "Heads Wins !"
+elif [ $heads -lt $tails ]
+then
+	echo "Tails Wins !"
+fi
